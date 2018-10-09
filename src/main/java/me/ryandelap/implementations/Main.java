9dac92ac8875d6ea6caf65c5d9c85037cd5e4630
RyanDelap <akayungeagle@gmail.com>
2018-10-09 02:10:25 -0500

    Cleanup

--- src/main/java/me/ryandelap/implementations/Main.java
+++ src/main/java/me/ryandelap/implementations/Main.java
@@ -1,22 +1,8 @@
 package me.ryandelap.implementations;
 
-import me.ryandelap.implementations.datastructures.SinglyLinkedList;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args){
-        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
 
-        list.append(1);
-        list.append(234);
-        list.append(123452345);
-
-        for (Integer s : list) {
-            System.out.println(s);
-        }
     }
-    List list = new ArrayList<>();
 }
